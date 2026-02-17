7f3a3bfae4ac6c3363e11dc7853521a67cf23def
IsaacMrls <161041146+IsaacMrls@users.noreply.github.com>
2026-02-17 14:03:32 -0600

    Estadisticas Home
    Estadisticas de home

---
+++ EduPlan/src/app/screens/dashboard-alumno/dashboard-alumno.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-alumno',
+  imports: [],
+  templateUrl: './dashboard-alumno.html',
+  styleUrl: './dashboard-alumno.scss',
+})
+export class DashboardAlumno {
+
+}
---
+++ EduPlan/src/app/screens/dashboard-maestros/dashboard-maestros.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-maestros',
+  imports: [],
+  templateUrl: './dashboard-maestros.html',
+  styleUrl: './dashboard-maestros.scss',
+})
+export class DashboardMaestros {
+
+}
