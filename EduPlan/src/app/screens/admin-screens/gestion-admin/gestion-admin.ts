9106f962374a0b1f6cdb83e9334770626eb80a42
Missael1232 <missatemuali@gmail.com>
2026-02-27 14:20:37 -0600

    Creación de los componentes de admin

---
+++ EduPlan/src/app/screens/admin-screens/gestion-admin/gestion-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-admin',
+  imports: [],
+  templateUrl: './gestion-admin.html',
+  styleUrl: './gestion-admin.scss',
+})
+export class GestionAdmin {
+
+}
---
+++ EduPlan/src/app/screens/admin-screens/solicitudes-admin/solicitudes-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitudes-admin',
+  imports: [],
+  templateUrl: './solicitudes-admin.html',
+  styleUrl: './solicitudes-admin.scss',
+})
+export class SolicitudesAdmin {
+
+}
