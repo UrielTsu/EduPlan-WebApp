85ec25e2125fb76c0a054ffb1755dfc3aaf1d08f
UrielTsu <142840957+UrielTsu@users.noreply.github.com>
2026-03-03 10:27:34 -0600

    error cursos

--- EduPlan/src/app/app.routes.ts
+++ EduPlan/src/app/app.routes.ts
@@ -36,7 +36,7 @@ export const routes: Routes = [
         component: AdminDashboard
       }
       ,{
-        path: 'cursos-d',
+        path: 'cursos-d/:id',
         component: CursosD
       },
         {
